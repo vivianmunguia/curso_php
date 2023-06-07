f18f9a6882165dd876cddaa21c15eae8eb5b51b2
vivianmunguia <andy_munguia@hotmail.com>
2023-06-07 02:05:06 -0700

    The comentarios.php file was added with 3 sample comments

---
+++ comentarios.php
@@ -0,0 +1,12 @@
+<?php
+//Mi primer hola mundo
+echo "Hola mundo 1";
+
+#Mi segundo hola mundo
+echo "Hola mundo 2";
+
+/*
+    Autor: Vivian Munguia
+    Fecha: 6-Junio-2023
+*/
+echo "Hola mundo 3";
